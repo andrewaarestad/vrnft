ce14959c8ffe8c9c2a77fb6e6a8e650d478a4984
Andrew Aarestad <andrew@aa-engineering.com>
2021-10-16 22:42:33 -0500

    initial commit

---
+++ contracts/VRNFT.sol
@@ -0,0 +1,104 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "hardhat/console.sol";
+
+/**
+ * @title VRNFT
+ * @author @AndrewAarestad
+ */
+contract VRNFT is ERC721Enumerable, ReentrancyGuard, Ownable {
+    using Strings for uint256;
+
+    uint256 public constant MAX_TOKEN_ID = 8888;
+
+    bool public publicSaleActive = false;
+    string private _imageUriPrefix;
+    string private _uriPrefix;
+
+    mapping(uint256 => string) private tokenSeeds;
+
+    constructor() ERC721("Verifiably-Random NFT", "VRNFT") {
+
+    }
+
+    function setBaseImageURI(string memory prefix) public onlyOwner {
+        _imageUriPrefix = prefix;
+    }
+
+    function setBaseURI(string memory prefix) public onlyOwner {
+        _uriPrefix = prefix;
+    }
+
+    function togglePublicSale() public onlyOwner {
+        publicSaleActive = !publicSaleActive;
+    }
+
+
+
+}
+
+/// [MIT License]
+/// @title Base64
+/// @notice Provides a function for encoding some bytes in base64
+/// @author Brecht Devos <brecht@loopring.org>
+library Base64 {
+    bytes internal constant TABLE = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    /// @notice Encodes some bytes to the base64 representation
+    function encode(bytes memory data) internal pure returns (string memory) {
+        uint256 len = data.length;
+        if (len == 0) return "";
+
+        // multiply by 4/3 rounded up
+        uint256 encodedLen = 4 * ((len + 2) / 3);
+
+        // Add some extra buffer at the end
+        bytes memory result = new bytes(encodedLen + 32);
+
+        bytes memory table = TABLE;
+
+        assembly {
+            let tablePtr := add(table, 1)
+            let resultPtr := add(result, 32)
+
+            for {
+                let i := 0
+            } lt(i, len) {
+
+            } {
+                i := add(i, 3)
+                let input := and(mload(add(data, i)), 0xffffff)
+
+                let out := mload(add(tablePtr, and(shr(18, input), 0x3F)))
+                out := shl(8, out)
+                out := add(out, and(mload(add(tablePtr, and(shr(12, input), 0x3F))), 0xFF))
+                out := shl(8, out)
+                out := add(out, and(mload(add(tablePtr, and(shr(6, input), 0x3F))), 0xFF))
+                out := shl(8, out)
+                out := add(out, and(mload(add(tablePtr, and(input, 0x3F))), 0xFF))
+                out := shl(224, out)
+
+                mstore(resultPtr, out)
+
+                resultPtr := add(resultPtr, 4)
+            }
+
+            switch mod(len, 3)
+            case 1 {
+                mstore(sub(resultPtr, 2), shl(240, 0x3d3d))
+            }
+            case 2 {
+                mstore(sub(resultPtr, 1), shl(248, 0x3d))
+            }
+
+            mstore(result, encodedLen)
+        }
+
+        return string(result);
+    }
+}
+
